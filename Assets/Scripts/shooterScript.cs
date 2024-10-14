bd710393607432a35a2f71bf498d339b0e2958fd
Steffen Møgelmose <steffen@mogelmose.org>
2024-10-14 09:27:06 +0200

    Added gun

---
+++ Assets/Scripts/shooterScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class shooterScript : MonoBehaviour
+{
+    
+	public GameObject bullet;
+	public Transform spawnPosition;
+	
+	
+	// Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
